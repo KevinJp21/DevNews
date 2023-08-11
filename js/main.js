8a6a0c8410b01a07ae6c69011aefa0c4826173ad
KevinJp21 <kevinjp821@gmail.com>
2023-08-10 19:31:06 -0500

    Modo oscuro listo

--- js/main.js
+++ js/main.js
@@ -2,10 +2,19 @@
 const DarkModeOn = document.getElementById('DarkModeOn');
 DarkModeOn.addEventListener('click', () => {
   document.body.classList.add('dark-mode');
+  localStorage.setItem('modo', 'oscuro');
 });
 
 const LightModeOn = document.getElementById('LightModeOn');
 LightModeOn.addEventListener('click', () => {
   document.body.classList.remove('dark-mode');
+  localStorage.setItem('modo', 'claro');
 });
 
+// Verifica el modo al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+  const modoGuardado = localStorage.getItem('modo');
+  if (modoGuardado === 'oscuro') {
+    document.body.classList.add('dark-mode');
+  }
+});
